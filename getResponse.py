39fd7ba1c6c38d819c8912950deb232cd03274f7
Alan Finke <afinke@verseon.com>
2024-10-23 13:32:59 -0700

    design change

--- getResponse.py
+++ getResponse.py
@@ -2,7 +2,7 @@
 from openai import OpenAI;
 import os;
 
-def getResponse(name, denomination, type):
+def getResponse(name, denomination, contype):
 
     client = OpenAI()
     OpenAI.api_key = os.environ["OPENAI_API_KEY"] ;
@@ -10,20 +10,21 @@ def getResponse(name, denomination, type):
     #outputType = "an absolution";
     #if type == "blessing":
     #    outputType = "a blessing";
+    #print("type="+contype+ ",denomination=" + denomination + ", name="+name);
         
     
     role = "You are a " + denomination;
     
     content = "";
-    if type == "blessing":
+    if contype == "blessing":
         content = "Write a blessing for someone for the act of " + name + " and write the response in the first person";
     else:
         content = "Write an absolution for someone for someone who committed the sin of " + name + " and write the response in the first person";
     
     #content = "Write " + outputType + " for someone who committed the act of " + name + " and write the response in the first person";
 
-    print("role - " +role);
-    print("content - " + content);
+    #print("role - " +role);
+    #print("content - " + content);
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
